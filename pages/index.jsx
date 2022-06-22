5f082ca5cbdb537689cbfaa82e45eb3f9a07a7dc
truongvukt2000 <truongvukt2000@gmail.com>
2022-06-22 15:44:30 +0700

    prop key list compaign

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import factory from '../ethereum/factory';
-import Layout from '../components/layout';
-import { Link } from '../routes';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import factory from "../ethereum/factory";
+import Layout from "../components/layout";
+import { Link } from "../routes";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 const CampaignIndex = ({ campaigns }) => {
   return (
     <Layout>
-      <div style={{display: "flex", flexDirection: 'column', alignItems: 'center'}}>
-        <Typography variant='h4' style={{marginTop: '10px'}}>Campaign List</Typography>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" style={{ marginTop: "10px" }}>
+          Campaign List
+        </Typography>
         {campaigns.map((item, i) => {
           return (
-            <Card sx={{ maxWidth: 1500 }} style={{ marginTop: "20px", width: '1300px' }}>
+            <Card
+              key={i}
+              sx={{ maxWidth: 1500 }}
+              style={{ marginTop: "20px", width: "1300px" }}
+            >
               <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
                   Address
                 </Typography>
                 <Typography variant="h5" component="div">
@@ -30,22 +46,20 @@ const CampaignIndex = ({ campaigns }) => {
                 </Link>
               </CardActions>
             </Card>
-          )
+          );
         })}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 export async function getServerSideProps() {
-  const campaigns = await factory.methods
-    .getDeployedContracts()
-    .call();
+  const campaigns = await factory.methods.getDeployedContracts().call();
 
   return {
     props: {
-      campaigns
-    }
+      campaigns,
+    },
   };
 }
 
