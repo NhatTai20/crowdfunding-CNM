84ac8f3b0bf5a2656ceb8478187019b1de5e60e4
NhatTai20 <ntai20111@gmail.com>
2022-06-24 22:10:57 +0700

    redeploy

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import campaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   campaignFactory.abi,
-  '0xF97b29E3F0FaD7EA860b60F9ad1456580FC0773d'
+  '0xc5C8530f3a94AC591E590Bdd0F5E8d155eF594c6'
 );
 
 export default instance;
